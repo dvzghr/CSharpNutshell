979279868f1c3ddc5ab423e1aed46ddc837c0c36
ths_hr <ths_hr@hotmail.com>
2016-12-08 15:38:46 +0100

    Add project files.

---
+++ CoAndContraVariance/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoAndContraVariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Animal animalCat = new Cat();
+
+            Cat cat = new Cat();
+            Cat catAnimal = new Animal();   //Compiler error
+
+            //=====================================
+
+            IEnumerable<Animal> animals = new List<Cat>();  //COVARIANCE
+            IEnumerable<Cat> cats = new List<Animal>();     //Compiler error
+
+            //=====================================
+
+            IGenericInVariant<Animal> animalInVariant = new HandleCatsInVariant();  //Compiler error
+            IGenericCovariant<Animal> animalCovariant = new HandleCatsCovariant();  //COVARIANCE
+
+            IGenericInVariant<Cat> catInVariant = new HandleAnimalsInVariant();     //Compiler error
+            IGenericInVariant<Cat> catCovariant = new HandleAnimalsCovariant();     //Compiler error
+        }
+    }
+
+    interface IGenericInVariant<T>
+    {
+        T GetAnimal();
+    }
+
+    interface IGenericCovariant<out T>
+    {
+        T GetAnimal();
+    }
+
+    public class Animal { }
+
+    public class Cat : Animal { }
+
+    public class HandleAnimalsCovariant : IGenericCovariant<Animal>
+    {
+        public Animal GetAnimal()
+        {
+            return new Animal();
+        }
+    }
+
+    public class HandleCatsCovariant : IGenericCovariant<Cat>
+    {
+        public Cat GetAnimal()
+        {
+            return new Cat();
+        }
+    }
+    
+    public class HandleAnimalsInVariant : IGenericInVariant<Animal>
+    {
+        public Animal GetAnimal()
+        {
+            return new Animal();
+        }
+    }
+
+    public class HandleCatsInVariant : IGenericInVariant<Cat>
+    {
+        public Cat GetAnimal()
+        {
+            return new Cat();
+        }
+    }
+}
