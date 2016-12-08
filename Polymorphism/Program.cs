d3a3c992ea9b52a289897bdaa700dfd775edcd97
ths_hr <ths_hr@hotmail.com>
2016-12-08 16:44:42 +0100

    added polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("\nbaseClass = new BaseClass()\n-------------");
+            BaseClass baseClass = new BaseClass(2);
+            baseClass.Read();
+            baseClass.Write();
+            baseClass.WritePrivate();   //Private
+            baseClass.WriteVirtual();
+
+            x = baseClass.SimpleNumber;
+            y = baseClass.ProtectedNumber;  //Protected
+
+
+            Console.WriteLine("\nbaseClass = new DerivedClass()\n-------------");
+            BaseClass baseDerived = new DerivedClass();
+            baseDerived.Read();
+            baseDerived.Write();
+            baseDerived.WritePrivate();     //Private
+            baseDerived.WriteVirtual();
+            baseDerived.DerivedMethod();    //Compiler error
+
+            x = baseDerived.SimpleNumber;
+            y = baseDerived.ProtectedNumber; //Protected
+
+
+            Console.WriteLine("\nderivedClass = new DerivedClass()\n-------------");
+            DerivedClass derived = new DerivedClass();
+            derived.Read();
+            derived.Write();
+            derived.WritePrivate();     //Private
+            derived.WriteVirtual();
+            derived.DerivedMethod();
+
+            x = baseDerived.SimpleNumber;
+            y = baseDerived.ProtectedNumber //Protected
+
+
+            Console.WriteLine("\nibaseClass = new BaseClass()\n-------------");
+            IInterface iBaseClass = new BaseClass(3);
+            iBaseClass.Read();
+            iBaseClass.Write();
+            //NoVirtualMethod!!!
+
+            Console.WriteLine("=============");
+            Console.WriteLine("*************");
+
+
+            Console.WriteLine("\nderivedAbstract = new DerivedFromAbstractClass()\n-------------");
+            var derivedAbstract = new DerivedFromAbstractClass();
+
+
+            Console.ReadKey();
+
+            var baseClass2 = new BaseClass(33);
+            IInterface interfaceClass = baseClass2;
+            var baseClass3 = (BaseClass)interfaceClass;
+            Console.WriteLine(baseClass3.SimpleNumber);
+
+        }
+    }
+
+    public class BaseClass : IInterface
+    {
+        public BaseClass(int x)
+        {
+            SimpleNumber = x;
+            ProtectedNumber = x + 1;
+            WritePrivate();
+        }
+
+        public int SimpleNumber { get; set; }
+        protected int ProtectedNumber { get; set; }
+
+        public void Write()
+        {
+            Console.WriteLine("Write BaseClass");
+        }
+
+        public void Read() { }
+
+        private void WritePrivate()
+        {
+            Console.WriteLine("WritePrivate BaseClass");
+        }
+
+        public virtual void WriteVirtual()
+        {
+            Console.WriteLine("WriteVirtual BaseClass");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public DerivedClass() : base(2)
+        {
+            ProtectedNumber = 100;
+            //WritePrivate(); //Base: Private
+        }
+
+        public void DerivedMethod()
+        {
+            Console.WriteLine(ProtectedNumber);
+        }
+
+        public int DerivedNumber { get; set; }
+
+        public void Write()
+        {
+            Console.WriteLine("Write DerivedClass");
+        }
+
+        public override void WriteVirtual()
+        {
+            Console.WriteLine("WriteVirtual DerivedClass");
+        }
+    }
+
+    interface IInterface
+    {
+        void Write();
+        void Read();
+    }
+
+    public abstract class AbstractClass
+    {
+        private int _broj;
+        public int BrojDrugi { get; set; }
+        protected int BrojProtected { get; set; }
+    }
+
+    public class DerivedFromAbstractClass : AbstractClass
+    {
+        public DerivedFromAbstractClass()
+        {
+
+        }
+    }
+
+}
---
+++ Polymorphism/Program2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program2
+    {
+        //static void Main(string[] args)
+        void Main()
+        {
+            XBaseClass bc = new XBaseClass();
+            XDerivedClass dc = new XDerivedClass();
+            XBaseClass bcdc = new XDerivedClass();
+
+            // The following two calls do what you would expect. They call
+            // the methods that are defined in BaseClass.
+            bc.Method1();
+            bc.Method2();
+            // Output:
+            // Base - Method1
+            // Base - Method2
+
+
+            // The following two calls do what you would expect. They call
+            // the methods that are defined in DerivedClass.
+            dc.Method1();
+            dc.Method2();
+            // Output:
+            // Derived - Method1
+            // Derived - Method2
+
+
+            // The following two calls produce different results, depending 
+            // on whether override (Method1) or new (Method2) is used.
+            bcdc.Method1();
+            bcdc.Method2();
+            // Output:
+            // Derived - Method1
+            // Base - Method2
+
+            //bcdc.Method3(); NOT ACCESSIBLE
+        }
+    }
+
+    class XBaseClass
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("Base - Method1");
+        }
+
+        public virtual void Method2()
+        {
+            Console.WriteLine("Base - Method2");
+        }
+    }
+
+    class XDerivedClass : XBaseClass
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Derived - Method1");
+        }
+
+        public new void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+
+        public void Method3()
+        {
+
+        }
+    }
+
+}
