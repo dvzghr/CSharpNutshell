e5342c4220dd9ee76646070ef7e8f49c484b0827
thshr <ths_hr@hotmail.com>
2017-05-17 18:13:02 +0200

    c#7 new feat

---
+++ CSharp7_NewFeatures/OutVariables.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//NEW FEATURE: using static
+using static System.Console;
+
+namespace CSharp7_NewFeatures
+{
+    public class OutVariables
+    {
+        public void Run()
+        {
+            //PRE C#7
+
+            //Can not use var declaration for out params
+            //var hour, min, sec;
+            int hour, min, sec;
+            GetTime(out hour, out min, out sec);
+            WriteLine($"{hour}:{min}:{sec}");
+
+            //C#7
+            GetTime(out int newHour, out int newMin, out int newSec);
+            WriteLine($"{newHour}:{newMin}:{newSec}");
+
+        }
+
+
+        public void GetTime(out int hour, out int minutes, out int seconds)
+        {
+            var currDateTime = DateTime.Now;
+            hour = currDateTime.Hour;
+            minutes = currDateTime.Minute;
+            seconds = currDateTime.Second;
+        }
+    }
+}
---
+++ CSharp7_NewFeatures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//NEW FEATURE: using static
+using static System.Console;
+
+namespace CSharp7_NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*** out variables ***");
+            new OutVariables().Run();
+            WriteLine("---------------------\n");
+
+
+            ReadKey();
+        }
+    }
+}
