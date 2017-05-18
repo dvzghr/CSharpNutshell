e854f9c419f17dffbbe361729e2b102992968544
thshr <ths_hr@hotmail.com>
2017-05-18 16:27:49 +0200

    new additons

---
+++ CSharp7_NewFeatures/ConditionalSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7_NewFeatures
+{
+    class ConditionalSwitch
+    {
+        public void Run()
+        {
+            Person boss = new Boss
+                          {
+                              Age = 50,
+                              Exp = 30,
+                              NumberManaged = 10,
+                              NumberOwned = 50
+                          };
+
+            switch (boss)
+            {
+                case Boss b when (b.NumberOwned > 10):
+                    WriteLine($"NoOwned: {b.NumberOwned} HIGH");
+                    break;
+                case Boss b when (b.NumberOwned <= 10):
+                    WriteLine($"NoOwned: {b.NumberOwned} LOW");
+                    break;
+                case Manager m:
+                    WriteLine($"NoManaged: {m.NumberManaged}");
+                    break;
+
+                case Person p:
+                   WriteLine($"Age: {p.Age}"); 
+                    break;
+                
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+        public int Exp { get; set; }
+    }
+
+    class Manager : Person
+    {
+        public int NumberManaged { get; set; }
+    }
+
+    class Boss : Manager
+    {
+        public int NumberOwned { get; set; }
+    }
+}
