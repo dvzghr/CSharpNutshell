238cb86400b949fc049ea7976591cacb1ef6db3e
thshr <ths_hr@hotmail.com>
2017-05-23 17:49:29 +0200

    using static

---
+++ StaticUsing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Something;
+using static Something.SomeClass;
+
+namespace StaticUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var someClass = new SomeClass();
+            //someClass.Print();
+
+            Print();
+        }
+    }
+}
+
+namespace Something
+{
+    class SomeClass
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+    }
+}
