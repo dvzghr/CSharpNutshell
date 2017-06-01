601fa27134ec4ad2e52b51180cd0236ed4b65b1b
thshr <ths_hr@hotmail.com>
2017-06-02 01:06:20 +0200

    addition to enhancements
    expression body methods

--- CSharp7_NewFeatures/Enhancements.cs
+++ CSharp7_NewFeatures/Enhancements.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
+using static System.Int32;
 
 namespace CSharp7_NewFeatures
 {
@@ -16,7 +17,7 @@ namespace CSharp7_NewFeatures
             WriteLine(bigNumber);
 
             //return by reference
-            int[] numbers = { 2, 7, 1, 9, 12, 8, 15 };
+            int[] numbers = {2, 7, 1, 9, 12, 8, 15};
             ref int position = ref Substitute(12, numbers);
             position = -12;
             WriteLine(numbers[4]);
@@ -31,14 +32,22 @@ namespace CSharp7_NewFeatures
                     throw new NullReferenceException("Old way");
                 WriteLine(name);
             }
-            catch { }
+            catch
+            {
+            }
 
             //C#7
             try
             {
                 WriteLine(name ?? throw new NullReferenceException("new way"));
             }
-            catch { }
+            catch
+            {
+            }
+
+
+            //Expression methods
+            ConvertStringToInt();
         }
 
         public ref int Substitute(int value, int[] numbers)
@@ -51,7 +60,61 @@ namespace CSharp7_NewFeatures
 
             throw new IndexOutOfRangeException("Not found!");
         }
+
+        void ConvertStringToInt()
+        {
+            var someString = "11";
+            int someNumberFromString = 0;
+
+            someNumberFromString = someString.ToIntFromString();
+            someNumberFromString = someString.ToIntWithDelegate();
+            someNumberFromString = someString.ToIntWithDelegateGeneric();
+
+            someNumberFromString = someString.ToIntWithDelegateGenericFinal();
+            someNumberFromString = someString.ToIntWithDelegateGenericFinal();
+        }
     }
+
+    static class StringExtension
+    {
+        public static int ToIntFromString(this string str)
+        {
+            return TryParse(str, out int number) ? number : 0;
+        }
+    }
+
+    static class DelegateExtension
+    {
+        private delegate bool TryParseDelegate(string s, out int i);
+
+        private static int ToInt(string s, TryParseDelegate func) => func(s, out int i) ? i : 0;
+
+        public static int ToIntWithDelegate(this string str) => ToInt(str, int.TryParse);
+    }
+
+    static class GenericExtension
+    {
+        private delegate bool TryParseDelegate<T>(string s, out T i);
+
+        private static int ToInt(string s, TryParseDelegate<int> func) => func(s, out int i) ? i : 0;
+        private static DateTime ToDate(string s, TryParseDelegate<DateTime> func) => func(s, out DateTime dt) ? dt : DateTime.Now;
+
+        public static int ToIntWithDelegateGeneric(this string str) => ToInt(str, int.TryParse);
+
+        public static DateTime ToDateWithDelegateGeneric(this string str) => ToDate(str, DateTime.TryParse);
+    }
+
+    static class GenericExtensionFinal
+    {
+        private delegate bool TryParseDelegate<T>(string s, out T i);
+        private static T To<T>(string s, TryParseDelegate<T> func) => func(s, out T i) ? i : default(T);
+
+
+        public static int ToIntWithDelegateGenericFinal(this string str) => To<int>(str, int.TryParse);
+
+        public static DateTime ToDateWithDelegateGenericFinal(this string str) => To<DateTime>(str, DateTime.TryParse);
+    }
+
 
     class Car
     {
